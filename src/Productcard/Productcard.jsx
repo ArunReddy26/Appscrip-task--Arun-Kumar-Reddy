76c878f4e60c2ce5ed25ba47d95dd2172d2d75d7
Arun Kumar <kundraarun171221@gmail.com>
2025-04-08 14:18:36 +0530

    pre-final-changes1

--- src/Productcard/Productcard.jsx
+++ src/Productcard/Productcard.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Productcard.css";
+import { FaHeart } from "react-icons/fa";
+import { CiHeart } from "react-icons/ci";
 
 function ProductCard({ product }) {
+
+  const [isClicked, setIsClicked] = useState(false);
+  function delefromwhislist() {
+    setIsClicked(false);
+
+  }
+
+  function addtowhislist() {
+
+    setIsClicked(true);
+
+  }
+
   return (
     <div className="product-card">
       <img src={product.image} alt={product.title} />
       <h3>{product.title}</h3>
+
       <p>${product.price}</p>
+      <div className="productcardsecond-div">
+        {
+          isClicked ? <FaHeart onClick={() => delefromwhislist()} className="favourite-wishlist-heart" /> : <CiHeart className="heart" onClick={() => addtowhislist()} />
+        }
+      </div>
     </div>
   );
 }
